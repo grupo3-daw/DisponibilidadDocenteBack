77cef5c74ea33e84bdf5fa9f0f17e83ecb863be1
Charlie <tarazona.charlie@gmail.com>
2019-05-30 08:15:47 -0500

    funciones de disponibilidad

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Teacher;
+use App\Availability;
 
 class AvailabilityController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $availability = new Availability();
+        
+        $availability->day = $request->day;
+        $availability->hour = $request->hour;
+        $availability->teacher_id = $request->teacher_id;
+
+        $availability->save();
+
+        return response()->json([
+            'message' => 'Successfully created availability!'
+        ], 201);
+    }
+
+    public function get($id)
+    {
+        $availabilities = Availability::where('teacher_id',$id)->get();
+
+        return response()->json([
+            'availabilities' => $availabilities
+        ], 201);
+    }
 }
